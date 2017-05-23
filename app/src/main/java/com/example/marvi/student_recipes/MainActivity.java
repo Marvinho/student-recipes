0e41405327919d32aca41f3f4e9f26942c8dac38
Marvin <s5651175@mail.zih.tu-dresden.de>
2017-05-23 19:11:57 +0200

    added Recyclerview

--- app/src/main/java/com/example/marvi/student_recipes/MainActivity.java
+++ app/src/main/java/com/example/marvi/student_recipes/MainActivity.java
@@ -2,25 +2,29 @@ package com.example.marvi.student_recipes;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private TextView recipesView;
+    private RecyclerView mRecyclerView;
+    private MyAdapter mAdapter;
+    private LinearLayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        recipesView = (TextView) findViewById(R.id.recipes);
-        ArrayList<String> list = new ArrayList<String>();
-        list.add("Pizza");
-        list.add("Kuchen");
-        list.add("hefkwhfkewfhwekfjhewfkwegfiewfhwefkgwfuwegfiwefgwi");
-        for(String i : list){
-            recipesView.append(i+"\n\n");
-        }
-
+        mRecyclerView = (RecyclerView) findViewById(R.id.recipe_recycler_view);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        ArrayList<Recipe> mRecipesList = new ArrayList<Recipe>();
+        Recipe mPitta = new Recipe("Pizza", "fhwiefhwui");
+        mRecipesList.add(mPitta);
+        mAdapter = new MyAdapter(mRecipesList);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
---
+++ app/src/main/java/com/example/marvi/student_recipes/MyAdapter.java
@@ -0,0 +1,62 @@
+package com.example.marvi.student_recipes;
+
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Marvi on 20.05.2017.
+ */
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    private ArrayList<Recipe> mRecipes;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        private TextView mRecipeTitle;
+        private Recipe mRecipe;
+
+
+        public ViewHolder(View v){
+            super(v);
+            mRecipeTitle = (TextView) v.findViewById(R.id.recipes);
+            v.setOnClickListener(this);
+        }
+
+        public void bindRecipe(Recipe recipe){
+            mRecipe = recipe;
+            mRecipeTitle.setText(mRecipe.getRecipeName());
+        }
+
+        @Override
+        public void onClick(View v) {
+            Log.d("RecyclerView","HFEWFHEWJ");
+        }
+    }
+
+    public MyAdapter(ArrayList<Recipe> recipes){
+        mRecipes = recipes;
+    }
+
+    @Override
+    public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View inflatedView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.recipe_list, parent, false);
+        return new ViewHolder(inflatedView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyAdapter.ViewHolder holder, int position) {
+        Recipe itemRecipe = mRecipes.get(position);
+        holder.bindRecipe(itemRecipe);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mRecipes.size();
+    }
+}
